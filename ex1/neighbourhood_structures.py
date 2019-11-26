d2de332c0f07befc5867a9c63288ed7007b8f2f2
Giulio <giulio.pace93@gmail.com>
2019-11-26 15:56:19 +0100

    OneBlockMove

--- ex1/neighbourhood_structures.py
+++ ex1/neighbourhood_structures.py
@@ -82,4 +82,72 @@ class DriverOneExchange:
 	def delta_eval(self, sol, pos, driver):
 		pass
 
+class OneBlockMove:
+
+	#moves a block from old position to new position. Everything else gets swifted accordingly to make room for it
+	def apply(self, sol, old_pos, new_pos):
+		if old_pos == new_pos:
+			print("old_pos and new_pos are the same")
+
+		if old_pos > new_pos:
+			size = len(sol.tour)
+			temp = np.zeroes(size)
+
+			for i in range(new_pos-1):
+				temp[i] = sol.tour[i]
+
+			temp[new_pos] = sol.tour[old_pos]
+
+			for i in range(old_pos - new_pos):
+				temp[new_pos + i + 1] = sol.tour[new_pos + i]
+
+			for i in range(size - old pos):
+				temp[old_pos + i] = sol.tour[old_pos + i]
+
+		if old_pos < new_pos:
+			size = len(sol.tour)
+			temp = np.zeroes(size)
+
+			for i in range(old_pos-1):
+				temp[i] = sol.tour[i]
+
+			for i in range(new_pos - old_pos):
+				temp[old_pos + i] = sol.tour[old_pos + i + 1]
+
+			temp[new_pos] = sol.tour[old_pos]
+
+			for i in range(size - new_pos):
+				temp[new_pos + i] = sol.tour[new_pos + i]
+
+
+	def move(self, sol, step_function="best_improvement", using_delta_eval=True):
+		if step_function == "random_improvement":
+			old_pos = random.randint(0, sol.inst.n-1)
+			new_pos = random.randint(0, sol.inst.n-1)
+			self.apply(sol, old_pos, new_pos)
+			return True
+		else:
+			best_so_far = sol
+			for old_pos in range(sol.inst.n):
+				for new_pos in range(sol.inst.n):
+					if using_delta_eval:
+						pass
+					else:
+						neighbour_solution = sol.copy()
+						self.apply(neighbour_solution, old_pos, new_pos)
+						#if neighbour_solution.is_better(best_so_far):
+						if neighbour_solution.calc_objective() < best_so_far.calc_objective():
+							if step_function == "next_improvement":
+								self.apply(sol, old_pos, new_pos)
+								return True
+							elif step_function == "best_improvement":
+								best_so_far = neighbour_solution
+			if best_so_far != sol:
+				sol.copy_from(best_so_far)
+				return True
+		return False
+
+	def delta_eval(self, sol, old_pos, new_pos):
+		pass
+
 
