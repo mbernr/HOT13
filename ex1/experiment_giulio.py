bbcdd53a20212917880b6dde4531c0a650fe831c
Giulio <giulio.pace93@gmail.com>
2019-11-27 18:31:40 +0100

    Experiments just so you know

--- ex1/experiment_giulio.py
+++ ex1/experiment_giulio.py
@@ -5,23 +5,26 @@ from neighbourhood_structures import *
 from local_search import *
 from grasp import *
 from vnd import *
+from simulated_annealing import *
 
 
 inst = HotInstance("instances/0010_k2.txt")
-# ns = DriverOneExchange()
-#ns = TourReversal()
+#ns = DriverOneExchange()
+ns = TourReversal()
+#ns = OneBlockMove()
+nsa = [TourReversal(), OneBlockMove(), DriverOneExchange()]
 
-ns = OneBlockMove()
+#---------------STUFF TO PLAY AROUND---------------
+multiplier = 1  #variable to play around with temperature
+alpha = 0.95
+max_iterations = 100000
+max_time = 15
+#----------------------------------------------------
 
-'''
-sol = grasp(inst,ns,using_delta_eval=False, max_iterations=100)
-print(sol)
-print(sol.obj())
-'''
+sol = construct_greedy(inst)
+#sol = construct_random_greedy(inst, 0.25)
+T = sol.inst.M * sol.inst.k *multiplier #temperature
 
-
-print()
-sol = construct_random_greedy(inst, 1.0)
 #sol = construct_greedy(inst)
 # sol = HotSolution(inst, drivers=[0,1,1,0,0,1,1,0,0,1])
 
@@ -29,23 +32,9 @@ print(sol)
 print(sol.obj())
 print()
 
-print("delta")
-local_search(sol, ns,
-    max_iterations=10,
-    step_function="best_improvement",
-    using_delta_eval=True)
+simulated_annealing(sol, nsa, T, alpha, max_iterations, max_time)
+
 print(sol)
 print(sol.obj())
 print()
 
-sol1 =sol.copy()
-# best_improvement, next_improvement, random_improvement
-print("no delta")
-local_search(sol1, ns,
-    max_iterations=10,
-    step_function="best_improvement",
-    using_delta_eval=False)
-
-print(sol1)
-print(sol1.obj())
-print()
