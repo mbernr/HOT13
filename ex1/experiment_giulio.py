8fd64303b807dba451c3c568eb207f5c7eb447d5
Giulio <giulio.pace93@gmail.com>
2019-11-27 12:57:51 +0100

    FINALLY

--- ex1/experiment_giulio.py
+++ ex1/experiment_giulio.py
@@ -1,31 +1,51 @@
 from hot_instance import *
 from hot_solution import *
 from construction_heuristics import *
+from neighbourhood_structures import *
+from local_search import *
+from grasp import *
+from vnd import *
 
 
 inst = HotInstance("instances/0010_k2.txt")
+# ns = DriverOneExchange()
+#ns = TourReversal()
 
-#print(i.k, i.L, i.n, i.m)
-#print(i.G.edges().data())
+ns = OneBlockMove()
 
-sol = HotSolution(inst)
-
-print("neighbourhood_search_driver_flip")
-for i in range(5):
-    if sol.neighbourhood_search_driver_flip(step_function="next_improvement"):
-        print(sol)
-        print(round(sol.calc_objective(),2))
-        print()
-    else:
-        print("No improvement found")
+'''
+sol = grasp(inst,ns,using_delta_eval=False, max_iterations=100)
+print(sol)
+print(sol.obj())
+'''
 
 
-print("neighbourhood_search_tour_swap")
-for i in range(5):
-    if sol.neighbourhood_search_tour_swap(step_function="random_improvement"):
-        print(sol)
-        print(round(sol.calc_objective(),2))
-        print()
-    else:
-        print("No improvement found")
-        print()
+print()
+sol = construct_random_greedy(inst, 1.0)
+#sol = construct_greedy(inst)
+# sol = HotSolution(inst, drivers=[0,1,1,0,0,1,1,0,0,1])
+
+print(sol)
+print(sol.obj())
+print()
+
+print("delta")
+local_search(sol, ns,
+    max_iterations=10,
+    step_function="best_improvement",
+    using_delta_eval=True)
+print(sol)
+print(sol.obj())
+print()
+
+sol1 =sol.copy()
+# best_improvement, next_improvement, random_improvement
+print("no delta")
+local_search(sol1, ns,
+    max_iterations=10,
+    step_function="best_improvement",
+    using_delta_eval=False)
+
+print(sol1)
+print(sol1.obj())
+print()
