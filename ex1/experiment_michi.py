7616c3e7c4584d696157560e66db07957e0d0387
Michael Bernreiter <mbernr@outlook.com>
2019-11-27 12:33:16 +0100

    refactored experiment_michi

--- ex1/experiment_michi.py
+++ ex1/experiment_michi.py
@@ -9,44 +9,68 @@ from vnd import *
 
 inst = HotInstance("instances/0010_k2.txt")
 # ns = DriverOneExchange()
-#ns = TourReversal()
+# ns = TourReversal()
 ns = OneBlockMove()
 
 '''
+
+# test grasp
+
 sol = grasp(inst,ns,using_delta_eval=False, max_iterations=100)
 print(sol)
 print(sol.obj())
+
+print()
 '''
 
 
-print()
+
+'''
+
+# test vnd 
 
 sol = construct_random_greedy(inst, 0.25)
 print(sol)
 print(sol.obj())
+
+print()
 
 vnd(sol, using_delta_eval=False)
 print(sol)
 print(sol.obj())
 
 '''
-# sol = construct_random_greedy(inst, 1.0)
-sol = construct_greedy(inst)
+
+
+# test delta eval
+
+sol = construct_random_greedy(inst, 1.0)
+# sol = construct_greedy(inst)
 # sol = HotSolution(inst, drivers=[0,1,1,0,0,1,1,0,0,1])
 
 print(sol)
 print(sol.obj())
 print()
 
-sol1 =sol.copy()
-# best_improvement, next_improvement, random_improvement
+copy = sol.copy()
+
 print("no delta")
 local_search(sol, ns,
-	max_iterations=10,
+	max_iterations=100,
 	step_function="best_improvement",
 	using_delta_eval=False)
 
 print(sol)
 print(sol.obj())
 print()
-'''
+
+print("with delta")
+local_search(copy, ns,
+	max_iterations=100,
+	step_function="best_improvement",
+	using_delta_eval=True)
+
+print(copy)
+print(copy.obj())
+print()
+
