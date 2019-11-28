fixed integer overflow

ex1/hot_solution.py
@@ -48,31 +48,27 @@ class HotSolution(Solution):
 		s += "\n "
 		for driver in self.drivers:
 			s += str(driver) + " "
-		if self.is_infeasible():
-			s += "\nAbove solution is infeasible."
 		return s
 
 
 	def calc_objective(self):
 		self.calc_driver_distances()
-		squared_error_per_driver = np.square(self.driver_distances)
-		squared_error = np.sum(squared_error_per_driver)
+		squared_error = 0
+		for i in range(self.inst.k):
+			squared_error += self.driver_distances[i]**2
 		self.obj_val = squared_error
 		self.obj_val_valid = True
 		return self.obj_val
 
 
 	def calc_driver_distances(self):
-		k = self.inst.k
-		n = self.inst.n
-		L = self.inst.L
-		self.driver_distances = np.full(k, -L)
-		for i in range(n-1):
+		self.driver_distances = [-self.inst.L for i in range(self.inst.k)]
+		for i in range(self.inst.n-1):
 			driver = self.drivers[i]
 			dist = self.inst.get_distance(self.tour[i], self.tour[i+1])
 			self.driver_distances[driver] += dist
-		driver = self.drivers[n-1]
-		dist = self.inst.get_distance(self.tour[n-1], self.tour[0])
+		driver = self.drivers[self.inst.n-1]
+		dist = self.inst.get_distance(self.tour[self.inst.n-1], self.tour[0])
 		self.driver_distances[driver] += dist
 
 	def rmse(self):

ex1/neighbourhood_structures.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import copy
 
 
 class TourReversal:
@@ -53,7 +54,7 @@ class TourReversal:
 			pos1, pos2 = min(pos1, pos2), max(pos1, pos2)
 
 			squared_error = sol.obj()
-			driver_distances = np.copy(sol.driver_distances)
+			driver_distances = copy.deepcopy(sol.driver_distances)
 
 			#1
 			dr = sol.drivers[(pos1-1)%sol.inst.n]
@@ -118,7 +119,7 @@ class DriverOneExchange:
 	def delta_eval(self, sol, pos, new_driver):
 		if sol.drivers[pos] == new_driver:
 			return sol.obj()
-		driver_distances = np.copy(sol.driver_distances)
+		driver_distances = copy.deepcopy(sol.driver_distances)
 		squared_error = sol.obj()
 		old_driver = sol.drivers[pos]
 		edge_weight = sol.inst.get_distance(sol.tour[pos], sol.tour[(pos+1)%sol.inst.n])
@@ -217,7 +218,7 @@ class OneBlockMove:
 				return tr.delta_eval(sol, old_pos, new_pos)
 			else:
 				squared_error = sol.obj()
-				driver_distances = np.copy(sol.driver_distances)
+				driver_distances = copy.deepcopy(sol.driver_distances)
 
 				#1
 				dr = sol.drivers[old_pos]