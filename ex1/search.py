0a85330c1581b99419c76b577f4475f7c221a697
Giulio <giulio.pace93@gmail.com>
2019-11-28 10:01:40 +0100

    local_search, vnd and simulated annealing are now one file called search

--- ex1/search.py
+++ ex1/search.py
@@ -1,8 +1,53 @@
-from neighbourhood_structures import*
+from neighbourhood_structures import *
 import random
 import time
 import math
 
+
+def local_search(sol, ns, max_iterations=math.inf, max_time=math.inf, step_function="best_improve", using_delta_eval=True):
+
+	if step_function not in ["best_improvement", "next_improvement", "random_improvement"]:
+		print("Error in local search: Invalid step function.")
+
+	iterations = 0
+	start_time = time.clock()
+
+	while ns.move(sol, step_function=step_function, using_delta_eval=using_delta_eval):
+
+		# checking if max number of iterations is exceeded
+		iterations += 1
+		if iterations >= max_iterations:
+			break
+
+		# checking if max time is exceeded
+		if time.clock()-start_time > max_time:
+			break
+
+
+def vnd(sol, max_iterations=1000, max_time=15*60, step_function="best_improvement", using_delta_eval=True):
+
+	if step_function not in ["best_improvement", "next_improvement", "random_improvement"]:
+		print("Error in vnd: Invalid step function.")
+
+	iterations = 0
+	start_time = time.clock()
+
+	ns = [TourReversal(), OneBlockMove(), DriverOneExchange()]
+	i = 0 # index of current neighbourhood
+
+	while(i < len(ns)):
+
+		if ns[i].move(sol, step_function=step_function, using_delta_eval=using_delta_eval):
+			i = 0
+		else:
+			i += 1
+
+		iterations += 1
+		if iterations >= max_iterations:
+			break
+
+		if time.clock()-start_time > max_time:
+			break
 
 def simulated_annealing(sol, nsa, T, alpha = 0.95, max_iterations=100000, max_time=15*60):
 	'''
