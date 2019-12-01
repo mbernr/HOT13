10997152760067b508abbc89e7649d38a8b9bde9
Giulio <giulio.pace93@gmail.com>
2019-12-01 16:21:31 +0100

    code stuff

--- ex1/order66.py
+++ ex1/order66.py
@@ -16,7 +16,7 @@ def empty_folder(path):
 	for f in files:
 		os.remove(f)
 
-def store_results(sol_file_path, res_file_path, inst_name, sol, running_time, avg=-1):
+def store_results(sol_file_path, res_file_path, inst_name, sol, running_time, avg=-1, std=-1):
 	infeas = False
 	if sol.is_infeasible():
 		sol_file_path += "infeasible_"
@@ -31,6 +31,8 @@ def store_results(sol_file_path, res_file_path, inst_name, sol, running_time, av
 	result_file.write(str(round(sol.rmse(),4)))
 	if avg != -1:
 		result_file.write(", " + str(round(avg,4)))
+	if std != -1:
+		result_file.write(", " + str(round(std,4)))
 	if infeas == True:
 		result_file.write(", infeasible")
 	result_file.write("\n")
@@ -38,29 +40,41 @@ def store_results(sol_file_path, res_file_path, inst_name, sol, running_time, av
 
 print("lets go")
 
-webbrowser.open('https://www.youtube.com/watch?v=sNjWpZmxDgg')
+#webbrowser.open('https://www.youtube.com/watch?v=sNjWpZmxDgg')
 
 inst_list = [
-	'0010_k1.txt', '0010_k2.txt', 
-	'0015_k1.txt', '0015_k2.txt', 
-	'0020_k1.txt', '0020_k2.txt', 
-	'0025_k1.txt', '0025_k2.txt', 
-	'0030_k1.txt', '0030_k2.txt', 
-	'1000_k1.txt', '1000_k2.txt', 
-	'1500_k1.txt', '1500_k2.txt', 
-	'2000_k1.txt', '2000_k2.txt', 
-	'2500_k1.txt', '2500_k2.txt', 
-	'3000_k1.txt', '3000_k2.txt', 
-	'a280_k1_1.txt', 'a280_k1_2.txt', 'a280_k2_1.txt', 'a280_k2_2.txt', 'a280_k3_1.txt', 'a280_k3_2.txt', 'a280_k4_1.txt', 'a280_k4_2.txt', 'a280_k5_1.txt', 'a280_k5_2.txt', 
-	'berlin52_k1_1.txt', 'berlin52_k1_2.txt', 'berlin52_k2_1.txt', 'berlin52_k2_2.txt', 'berlin52_k3_1.txt', 'berlin52_k3_2.txt', 'berlin52_k4_1.txt', 'berlin52_k4_2.txt', 'berlin52_k5_1.txt', 'berlin52_k5_2.txt', 
-	# 'rl5915_k1_1.txt', 'rl5915_k1_2.txt', 'rl5915_k2_1.txt', 'rl5915_k2_2.txt', 'rl5915_k3_1.txt', 'rl5915_k3_2.txt', 'rl5915_k4_1.txt', 'rl5915_k4_2.txt', 'rl5915_k5_1.txt', 'rl5915_k5_2.txt'
+	#'0010_k1.txt', '0010_k2.txt',
+	#'0015_k1.txt', '0015_k2.txt',
+	#'0020_k1.txt', '0020_k2.txt',
+	#'0025_k1.txt', '0025_k2.txt',
+	#'0030_k1.txt', '0030_k2.txt',
+	#'1000_k1.txt', '1000_k2.txt',
+	#'1500_k1.txt', '1500_k2.txt',
+	#'2000_k1.txt', '2000_k2.txt',
+	#'2500_k1.txt', '2500_k2.txt',
+	#'3000_k1.txt', '3000_k2.txt',
+	#'a280_k1_1.txt', 'a280_k1_2.txt',
+	#'a280_k2_1.txt', 'a280_k2_2.txt',
+	#'a280_k3_1.txt', 'a280_k3_2.txt',
+	#'a280_k4_1.txt', 'a280_k4_2.txt',
+	#'a280_k5_1.txt', 'a280_k5_2.txt',
+	#'berlin52_k1_1.txt', 'berlin52_k1_2.txt',
+	#'berlin52_k2_1.txt', 'berlin52_k2_2.txt',
+	#'berlin52_k3_1.txt', 'berlin52_k3_2.txt',
+	#'berlin52_k4_1.txt', 'berlin52_k4_2.txt',
+	#'berlin52_k5_1.txt', 'berlin52_k5_2.txt',
+	 'rl5915_k1_1.txt', 'rl5915_k1_2.txt',
+	#'rl5915_k2_1.txt', 'rl5915_k2_2.txt',
+	#'rl5915_k3_1.txt', 'rl5915_k3_2.txt',
+	#'rl5915_k4_1.txt', 'rl5915_k4_2.txt',
+	#'rl5915_k5_1.txt', 'rl5915_k5_2.txt'
 ]
 
 
-files = glob.glob('results/*')
-for f in files:
-	file = open(f, "w")
-	file.close()
+#files = glob.glob('results/*')
+#for f in files:
+#	file = open(f, "w")
+#	file.close()
 
 empty_folder("solutions/deterministic/*")
 empty_folder("solutions/grasp/*")
@@ -69,7 +83,7 @@ empty_folder("solutions/random/*")
 empty_folder("solutions/vnd/*")
 empty_folder("solutions/simulated_annealing/*")
 
-print("the Jedi are dead")
+print("the Jedi are dead") #aka cleaned output files
 
 
 max_time = 2*60 #TODO CHANGE BEFORE RUN
@@ -77,6 +91,7 @@ max_iterations = 10000
 print("max_time: ", max_time, " max_iterations: ", max_iterations)
 
 for inst_name in inst_list:
+	time1 = time.time()
 	inst_path = "instances/" + inst_name
 	inst = HotInstance(inst_path)
 
@@ -97,23 +112,32 @@ for inst_name in inst_list:
 
 	aggregate_score = 0
 	nr = 10
+	avg = -1
+	std = -1
+	feas_solutions = []
 
 	for i in range(nr):
 
-		sol = construct_greedy(inst)
+		sol = construct_random_greedy(inst, 0.25)
 		if i==0:
 			best_sol = sol.copy()
 		else:
 			if best_sol.obj() > sol.obj():
 				best_sol.copy_from(sol)
-		aggregate_score += sol.rmse()
+		if sol.is_infeasible() == False:
+			feas_solutions.append(sol.rmse())
 
-	avg = aggregate_score/nr
+
+	feas_solutions = np.array(feas_solutions)
+	if len(feas_solutions)!= 0:
+		avg = np.mean(feas_solutions)
+		std = np.std(feas_solutions)
+
 	sol_file_path = "solutions/random/"
 	res_file_path = "results/random.txt"
 	running_time = time.process_time() - starting_time
 
-	store_results(sol_file_path, res_file_path, inst_name, best_sol, running_time, avg)
+	store_results(sol_file_path, res_file_path, inst_name, best_sol, running_time, avg=avg, std=std)
 
 
 	#----------------LOCAL SEARCH----------------------
@@ -145,7 +169,7 @@ for inst_name in inst_list:
 	starting_time = time.process_time()
 
 	sol.copy_from(det_sol)
-	ns = TourReversal() 
+	ns = TourReversal()
 
 	sol = grasp(inst, ns, max_iterations=1000, max_time=max_time)
 
@@ -190,7 +214,7 @@ for inst_name in inst_list:
 
 	#--------------ALL DONE----------------------
 	print(inst_name, ": all done")
-
-
+	print("time: ", round((time.time() - time1)/60, 2), "min")
+	print()
 
 print("saul goodman")
